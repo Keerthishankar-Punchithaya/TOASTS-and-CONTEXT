f8198171cdd49c944eb0e9a9e1f48128cec4bc60
Keerthishankar Punchithaya <punchithayakeerthishankar@gmail.com>
2021-08-27 21:23:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/toastsandcontext/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.toastsandcontext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_toast.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+//            Toast.makeText(applicationContext, "Hi,I'm a toast!", Toast.LENGTH_LONG).show()
+
+            Toast(this).apply {
+                duration = Toast.LENGTH_LONG
+                view = layoutInflater.inflate(R.layout.custom_toast, clToast)
+                show()
+            }
+        }
+    }
+}
